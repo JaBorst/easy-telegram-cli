45eb7170e6524dba42bfd062883205276b024d10
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2020-03-17 11:51:43 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='easy-telegram',
+    version='0.1',
+    packages=['easy-telegram'],
+    url='',
+    license='',
+    author='Janos Borst',
+    author_email='borst@informatik.uni-leipzig.de',
+    description='A very simple Python Wrapper around the Telegram API for sending Text messages from a python program and from Commandline.'
+)
